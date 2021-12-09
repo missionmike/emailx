4d80b1730279a0988b14b0bf74c6a970cc462d42
Michael Dinerstein <md@missionmike.dev>
2021-12-08 17:58:52 -0800

    Setup output processing and CSS inliner

--- src/generate.tsx
+++ src/generate.tsx
@@ -4,24 +4,47 @@ import { ServerStyleSheet } from "styled-components";
 import prettier from "prettier";
 import { renderToString } from "react-dom/server";
 import { writeFileSync } from "fs";
-
-console.log("Init.");
+import EmailTemplate from "./helpers/template";
+import inlineCss from "inline-css";
 
 Emails.map((Email) => {
   const sheet = new ServerStyleSheet();
+  const template = new EmailTemplate();
+  const title = ""; // TODO
 
   try {
-    let html = renderToString(sheet.collectStyles(<Email />));
-    const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
-    html = styleTags + html;
+    const innerContentWithClasses = renderToString(
+      sheet.collectStyles(<Email />)
+    );
+    const styleTags = sheet.getStyleTags();
+    const doctype = template.getDoctype();
+    const htmlTag = template.getHtmlTag();
+    const htmlHead = template.getHtmlHead({ title, styleTags });
+    const htmlBody = template.getHtmlBody({
+      innerHtml: innerContentWithClasses,
+    });
 
-    let htmlWDoc = "<!DOCTYPE html>" + html;
-    let prettyHtml = prettier.format(htmlWDoc, { parser: "html" });
+    let html = "";
+    html += doctype;
+    html += htmlTag;
+    html += htmlHead;
+    html += htmlBody;
+    html += "</html>";
+
+    let prettyHtml = prettier.format(html, { parser: "html" });
     let outputFile = `./output/${Email.name}.html`;
 
-    writeFileSync(outputFile, prettyHtml);
+    const inlineCssOptions = {
+      url: __filename,
+      removeStyleTags: false,
+    };
 
-    console.log(`Wrote ${outputFile}`);
+    inlineCss(prettyHtml, inlineCssOptions).then(function (inlinedCssHtml) {
+      writeFileSync(outputFile, inlinedCssHtml);
+      console.log(`Wrote ${outputFile}`);
+    });
+
+    // writeFileSync(outputFile, prettyHtml);
   } catch (error) {
     console.error("Generate error: " + error);
   } finally {
