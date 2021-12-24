f8de8f31859fa5c379aa8696ec3554247a112d36
Michael Dinerstein <md@missionmike.dev>
2021-12-23 17:49:58 -0800

    Update style organization

--- src/css/GlobalStyleReset.tsx
+++ src/css/GlobalStyleReset.tsx
@@ -38,18 +38,39 @@ const GlobalStyleReset = createGlobalStyle`
     padding: 0;
     width: 100% !important;
   }
-  
-  /* Begin custom styles */
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 1rem;
-    line-height: 1.7rem;
+
+  #outlook a {
+    padding: 0;
   }
-  
-  p {
-    margin: 0;
-    padding: 1rem;
-  }  
+  /* Force Outlook to provide a "view in browser" menu link. */
+  body {
+      width: 100% !important;
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+      margin: 0;
+      padding: 0;
+  }
+  /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */
+  .ExternalClass {
+      width: 100%;
+  }
+  /* Force Hotmail to display emails at full width */
+  .ExternalClass,
+  .ExternalClass p,
+  .ExternalClass span,
+  .ExternalClass font,
+  .ExternalClass td,
+  .ExternalClass div {
+      line-height: 100%;
+  }
+  /* Force Hotmail to display normal line spacing. More on that: http://www.emailonacid.com/forum/viewthread/43/ */
+  #backgroundTable {
+      margin: 0;
+      padding: 0;
+      width: 100% !important;
+      line-height: 100% !important;
+  }
+  /* End reset */
 `;
 
 export { GlobalStyleReset };
--- src/generator/EmailTemplate.tsx
+++ src/generator/EmailTemplate.tsx
@@ -21,42 +21,7 @@ class EmailTemplate {
         <style
           type="text/css"
           dangerouslySetInnerHTML={{
-            __html: `
-            #outlook a {
-                padding: 0;
-            }
-            /* Force Outlook to provide a "view in browser" menu link. */
-            body {
-                width: 100% !important;
-                -webkit-text-size-adjust: 100%;
-                -ms-text-size-adjust: 100%;
-                margin: 0;
-                padding: 0;
-            }
-            /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */
-            .ExternalClass {
-                width: 100%;
-            }
-            /* Force Hotmail to display emails at full width */
-            .ExternalClass,
-            .ExternalClass p,
-            .ExternalClass span,
-            .ExternalClass font,
-            .ExternalClass td,
-            .ExternalClass div {
-                line-height: 100%;
-            }
-            /* Force Hotmail to display normal line spacing. More on that: http://www.emailonacid.com/forum/viewthread/43/ */
-            #backgroundTable {
-                margin: 0;
-                padding: 0;
-                width: 100% !important;
-                line-height: 100% !important;
-            }
-            /* End reset */
-            
-            ${this.stripHtml(styleTags)}
-            `,
+            __html: this.stripHtml(styleTags),
           }}
         />
       </head>
